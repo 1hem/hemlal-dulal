0f1375b68cde83846e12f8b9ce97c7d923ff1d72
Hemlal Dulal <dulalhemlal986@gmail.com>
2023-04-02 13:31:28 +0545

    this commit
    main

--- js/index.js
+++ js/index.js
@@ -1,55 +1,51 @@
-
-        var somosaPrice =  document.getElementById("inputprice").value
-        var somatotal =  document.getElementById("total")
-        var addbtn = document .getElementById("right-button")
-        var decbtn = document .getElementById("left-button")
-        var somosaPrice =  document.getElementById("inputprice").value
-        var somatotal =  document.getElementById("total")
-        var addbtntea = document .getElementById("right-button-tea")
-        var decbtntea = document .getElementById("left-button-tea")
-        var gTotal = document .getElementById("grand-total")
+var somosaPrice = document.getElementById("inputprice").value;
+var somatotal = document.getElementById("total");
+var addbtn = document.getElementById("right-button");
+var decbtn = document.getElementById("left-button");
+var somosaPrice = document.getElementById("inputprice").value;
+var somatotal = document.getElementById("total");
+var addbtntea = document.getElementById("right-button-tea");
+var decbtntea = document.getElementById("left-button-tea");
+var gTotal = document.getElementById("grand-total");
 let cout = 0;
 
-        addbtn.addEventListener("click",()=>{
-            cout++
-            document.getElementById("counter-container").innerHTML= cout;
-             
-        })
+addbtn.addEventListener("click", () => {
+  cout++;
+  document.getElementById("counter-container").innerHTML = cout;
+});
 
-        decbtn.addEventListener("click",()=>{
-            cout--
-            document.getElementById("counter-container").innerHTML= cout;
-             
-        })
-        addbtntea.addEventListener("click",()=>{
-            cout++
-            document.getElementById("tea-counter-container").innerHTML= cout;
-             
-        })
+decbtn.addEventListener("click", () => {
+  cout--;
+  document.getElementById("counter-container").innerHTML = cout;
+});
+addbtntea.addEventListener("click", () => {
+  cout++;
+  document.getElementById("tea-counter-container").innerHTML = cout;
+});
 
-        decbtntea.addEventListener("click",()=>{
-            cout--
-            document.getElementById("tea-counter-container").innerHTML= cout;
-             
-        })
-        gTotal.addEventListener("click",()=>{
-            var somosaPrice =  document.getElementById("inputprice").value
-            var teaPrice =  document.getElementById("inputpricetea").value
-           
+decbtntea.addEventListener("click", () => {
+  cout--;
+  document.getElementById("tea-counter-container").innerHTML = cout;
+});
+gTotal.addEventListener("click", () => {
+  var somosaPrice = document.getElementById("inputprice").value;
+  var teaPrice = document.getElementById("inputpricetea").value;
 
- let val=  document.getElementById("counter-container").innerText
- let teaval=  document.getElementById("tea-counter-container").innerText
- 
-   let somosa =  document.getElementById("total-somosa").innerHTML = val*somosaPrice
-    let  tea =  document.getElementById("total-tea").innerHTML = teaval*  teaPrice;
-    let tptal_without =  document.getElementById("total-without-tax").innerHTML = somosa+ tea;
-   let vatAmount = 13/100*tptal_without;
-    document.getElementById("vat").innerHTML = vatAmount;
+  let val = document.getElementById("counter-container").innerText;
+  let teaval = document.getElementById("tea-counter-container").innerText;
 
-       let tipAmount =  10/100 * tptal_without;
-       document.getElementById("tip").innerHTML =  tipAmount
-       document.getElementById("grand-total").innerHTML = tptal_without+  tipAmount +  vatAmount
-        
-    
-    })
-//    
+  let somosa = (document.getElementById("total-somosa").innerHTML =
+    val * somosaPrice);
+  let tea = (document.getElementById("total-tea").innerHTML =
+    teaval * teaPrice);
+  let tptal_without = (document.getElementById("total-without-tax").innerHTML =
+    somosa + tea);
+  let vatAmount = (13 / 100) * tptal_without;
+  document.getElementById("vat").innerHTML = vatAmount;
+
+  let tipAmount = (10 / 100) * tptal_without;
+  document.getElementById("tip").innerHTML = tipAmount;
+  document.getElementById("totalShow").innerHTML =
+    tptal_without + tipAmount + vatAmount;
+});
+//
